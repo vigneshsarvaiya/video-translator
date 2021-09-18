cc37c62e2fd3244ab44e31a6e5da75d0449d3aa1
vigneshsarvaiya <vigneshsarvaiya@gmail.com>
2021-09-18 17:40:03 +0530

    My first commit

--- app.py
+++ app.py
@@ -624,4 +624,4 @@ def contact():
     return render_template('contact.html')
 
 if __name__== "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
---
+++ env/Lib/site-packages/flask_gtts/__init__.py
@@ -0,0 +1,2 @@
+from flask_gtts.main import gtts  # noqa
+from flask_gtts.about import (__license__, __version__, __author__, __doc__, __email__) # noqa
